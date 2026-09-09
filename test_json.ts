8d1e87e2f5d1a32e67cb20dd635e9ac7ef51382d
Firoz Mohammad <firozm613@gmail.com>
2026-09-09 13:34:45 +0530

    fix: log actual kite error for first minute strategy entry failure & clean up temp scripts

--- test_json.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from './src/database/db';
-async function main() {
-  const st = await prisma.strategy.findFirst({ where: { name: 'OH PREOPEN 15 MIN' } });
-  if (st) {
-    console.log(JSON.stringify(JSON.parse(st.configJson), null, 2));
-  }
-}
-main();
